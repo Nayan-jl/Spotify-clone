a7d609213430abe902a37c126e8715e50bbd9545
Nayan Jyoti Lekharu <nayanjyotilekharu@gmail.com>
2026-04-23 11:02:10 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ import NowPlaying from './components/NowPlaying';
 import Player from './components/Player';
 
 const App = () => {
+  const [currentSong, setCurrentSong] = useState({
+    title: "I Want You (Forever) - Josh Butler Remix - Radio Edit",
+    artist: "Carl Cox, Josh Butler",
+    img: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?w=800",
+    playlistName: "Techno"
+  });
   return (
-    <div className={styles.wrapper}>
+   <div className={styles.wrapper}>
       <TopNav />
       <div className={styles.mainLayout}>
-        <MainContent  />
-        <NowPlaying />
+        <Sidebar />
+        <MainContent onSelect={setCurrentSong} />
+        <NowPlaying song={currentSong} />
       </div>
-      <Player />
+      <Player song={currentSong} />
     </div>
   )
 }
