3429994c9c27ffdca15cbcef7151bcffbf8bed6c
Nayan Jyoti Lekharu <nayanjyotilekharu@gmail.com>
2026-04-23 17:19:59 +0530

    MainContent.jsx updated

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -12,6 +12,17 @@ const MainContent = ({ onSelect }) => {
     { name: "Intentions", img: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=100" },
     { name: "Justin Bieber", img: "https://images.unsplash.com/photo-1549834125-82d3c48159a3?w=100" }
   ];
+    const jumpBackIn = [
+    { title: "Khat", desc: "Navjot Ahuja", img: "https://images.unsplash.com/photo-1470225620780-dba8ba36b745?w=200" },
+    { title: "New World", desc: "Argy", img: "https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=200" },
+    { title: "Atomic Habits Audiobook", desc: "By JAGODA GASZYNSKA⭐", img: "https://images.unsplash.com/photo-1589829085413-56de8ae18c73?w=200" },
+    { title: "Badlapur (Original Motion Picture...", desc: "Sachin-Jigar", img: "https://images.unsplash.com/photo-1493225255756-d9584f8606e9?w=200" },
+    { title: "Nav", desc: "Nav", img: "https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?w=200" }  
+  ];
+  const albums = [
+    { title: "Kismat Konnection", desc: "Pritam, Atif Aslam", img: "https://images.unsplash.com/photo-1493225255756-d9584f8606e9?w=200" },
+    { title: "Calvin Harris", desc: "Various Artists", img: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?w=200" }
+  ];
   return (
     <div></div>
   )
