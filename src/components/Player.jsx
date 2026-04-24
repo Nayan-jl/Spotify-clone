141367dbef3f5f6b450730eee51f77c1d3a2f9f5
Nayan Jyoti Lekharu <nayanjyotilekharu@gmail.com>
2026-04-24 19:33:43 +0530

    Player.jsx updated

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Play, Pause, SkipBack, SkipForward, Repeat, Shuffle, Volume2, PlusCircle, MonitorPlay, Mic2, ListMusic, Maximize2 } from 'lucide-react';
+import styles from './Player.module.css';
 
-const Player = () => {
+const Player = ({ song, isPlaying, onTogglePlay }) => {
   return (
-    <div>Player</div>
+     <footer className={styles.player}>
+         <div className={styles.songInfo}>
+        <img src={song.img} className={styles.thumb} alt="" style={song.isBlue ? { background: 'linear-gradient(135deg, #450af5, #c4efd9)' } : {}} />
+        <div className={styles.songTextInfo}>
+          <p className={styles.songName}>{song.title}</p>
+          <p className={styles.artist}>{song.artist}</p>
+        </div>
+        <PlusCircle size={16} className={styles.songActionBtn} />
+      </div>
+      
+     </footer>
   )
 }
 
